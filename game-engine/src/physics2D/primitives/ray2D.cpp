34902dfe3c60c51e9906f933f5381116573e8f98
eliran_laptop <eliranmerhavi123456@gmail.com>
2023-10-10 17:32:16 +0300

    removed physics2D code

--- game-engine/src/physics2D/primitives/ray2D.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "ray2D.h"
-
-physics2D::ray2D::ray2D(const glm::f32vec2& origin, const glm::f32vec2& direction) : 
-	_origin(origin), _direction(glm::normalize(direction))
-{
-}
