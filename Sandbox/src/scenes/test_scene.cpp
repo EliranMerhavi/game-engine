c6ddf070ff8a73a2532a472594c39b552ad0ec3c
eliran_laptop <eliranmerhavi123456@gmail.com>
2023-10-23 18:01:20 +0300

    update test_scene.cpp

--- Sandbox/src/scenes/test_scene.cpp
+++ Sandbox/src/scenes/test_scene.cpp
@@ -11,8 +11,6 @@ test_scene::test_scene() :
 	camera(0.0f, game_engine::application::instance().width(), game_engine::application::instance().height(), 0.0f)
 	//camera(0.0f, game_engine::application::instance().width(), game_engine::application::instance().height(), 0.0f)
 {
-	for (int i = 1; i >= 1; i--)
-		quads.emplace_back(0, 0, i * 100, i * 100);
 }
 
 test_scene::~test_scene()
@@ -27,28 +25,9 @@ void test_scene::load_resources()
 {
 }
 
-rigidBody C(50, 0, 100, 0, 1, { 200,100 }, { 0,0 }, "square");
-rigidBody B(50, 0, 100, 0.01, 1, { 200, 300 }, { 1,0 }, "square");
-rigidBody D(50, 1, 50, 0.1, 0.07, { 640, 220 }, { -4, 0 }, "square");
-
-
-
-void displayRect(rigidBody A) {
-	renderer2D::rotated_quad(A.position.x - sqrt(2) / 2 * A.radius, A.position.y - sqrt(2) / 2 * A.radius, sqrt(2) * A.radius, sqrt(2) * A.radius, (A.angle - 0.7853) * 57.295);
-}
-
-
-bool collided = false;
 void test_scene::render()
 {	
-	renderer2D::set_camera(camera);
-	renderer2D::set_color({ 1, 0, 0, 1 });
-	renderer2D::rotated_quad(250, 250, 250, 250, degrees);
-	
-	degrees = fmod(degrees + time::delta_time, 360);
 
-	// x += 75 * time::delta_time;
-	// y += 75 * time::delta_time;
 }
 
 void test_scene::update()
