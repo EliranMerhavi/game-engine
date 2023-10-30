c34a1d91405de438f80d0bfe6a5028fd58a31bd1
Wakawaka <yitzhar@veredfamily.net>
2023-10-30 17:31:42 +0200

    Clean. This is basically what works minus the angular collision which needs work

--- Sandbox/src/scenes/test_scene.cpp
+++ Sandbox/src/scenes/test_scene.cpp
@@ -25,36 +25,14 @@ void test_scene::load_resources()
 {
 }
 
-rigidBody C(50, 0, 100, 0, 1, { 200,100 }, { 0,0 }, "square");
-rigidBody B(50, 0, 100, 0, 1, { 200, 300 }, { 0,0 }, "square");
-rigidBody D(50, 0, 50, 0, 0.07, { 640, 220 }, { -4, 0 }, "square");
 
-
-
-void displayRect(rigidBody A) {
-	renderer2D::rotated_quad(A.position.x - sqrt(2) / 2 * A.radius, A.position.y - sqrt(2) / 2 * A.radius, sqrt(2) * A.radius, sqrt(2) * A.radius, (A.angle - 0.7853) * 57.295);
-}
-
-
-bool collided = false;
 void test_scene::render()
 {	
-	renderer2D::set_camera(camera);
-	displayRect(C);
-	displayRect(B);
-	displayRect(D);
+
 }
 
 void test_scene::update()
 {
-	//Always put update first! There should probably be some universal loop that updates before we do any more logic.
-	C.updateRect(1);
-	B.updateRect(1);
-	D.updateRect(1);
-	collisions::angularCollision(C, B, 1);
-	collisions::angularCollision(D, B, 1);
-	collisions::angularCollision(C, D, 1);
-	//std::cout << A.omega << ", " << B.omega << "\n";
 
 }
 
