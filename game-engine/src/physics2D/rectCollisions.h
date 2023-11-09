9d18301364c02043571e89c4053ecc70741acc71
Wakawaka <yitzhar@veredfamily.net>
2023-11-09 13:38:22 +0200

    Implemented things into Eliran's architecture

---
+++ game-engine/src/physics2D/rectCollisions.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "ecs/components/quad.h"
+#include "pch.h"
+#include "scene/game_object.h"
+#include <limits>
+bool isColliding(game_engine::game_object& A, game_engine::game_object& B);
